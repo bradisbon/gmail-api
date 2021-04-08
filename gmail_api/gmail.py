ad36a34a419b6a4610c008a7ff1589f665361aa0
bradi <bradisbon@gmail.com>
2021-04-08 10:27:38 -0500

    Update: bump version to 0.2.0 gmail.py   - Add: get_token()

--- gmail_api/gmail.py
+++ gmail_api/gmail.py
@@ -1,13 +1,23 @@
 import base64, os
 from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
 from httplib2shim import Http
 from oauth2client import file
 from email.mime.text import MIMEText
 
-store = file.Storage(os.path.expanduser('~/.credentials/gmail_api_token.json'))
-creds = store.get()
-service = build('gmail', 'v1', http=creds.authorize(Http()))
+creds = Credentials.from_authorized_user_file(os.path.expanduser('~/.credentials/gmail_api_token.json'))
+service = build('gmail', 'v1', credentials=creds)
 user_id='me'
+
+
+def get_token(credentials_path):
+    flow = InstalledAppFlow.from_client_secrets_file(
+        credentials_path,
+        scopes = ['https://mail.google.com/']
+    )
+    token = flow.run_local_server(port=0)
+    return token.to_json()
 
 
 def create_message(sender, to, subject, message_text):
